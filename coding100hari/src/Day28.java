b39e9f3daa9ff85666a554de1cfc186377240b17
jeklin feronika sinaga <99625360+jeklinferonikasinaga@users.noreply.github.com>
2023-11-06 19:32:53 +0800

    Day 28 Pola Garis Tegak

---
+++ coding100hari/src/Day28.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Day28 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int jumlah_pola;
+        System.out.print("masukkan jumlah pola :");
+        jumlah_pola = sc.nextInt();
+        for (int i = 0; i <jumlah_pola ; i++) {
+            System.out.println(" *");
+
+        }
+        System.out.println();
+    }
+}
