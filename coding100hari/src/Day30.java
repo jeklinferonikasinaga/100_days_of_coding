7b9f0b9d71aeb416faa123ed89ad17222505c43c
jeklin feronika sinaga <99625360+jeklinferonikasinaga@users.noreply.github.com>
2023-11-08 21:20:38 +0800

    Day30 variabel

---
+++ coding100hari/src/Day30.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Day30 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+       String nama_depan = "fero";
+       String  nama_belakang = "sinaga";
+       int umur = 20;
+       double tinggi =1.5;
+       double berat_badan= 45;
+       char ukuran_baju = 'L';
+       boolean status_pernikahan = false ;
+
+
+
+
+       System.out.println(nama_depan);
+       System.out.println(nama_belakang);
+       System.out.println(umur);
+       System.out.println(tinggi+ "meter");
+       System.out.println(berat_badan+"kg");
+       System.out.println(ukuran_baju);
+       System.out.println(status_pernikahan);
+
+
+
+
+
+    }
+}
