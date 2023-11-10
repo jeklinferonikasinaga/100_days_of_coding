0cb178e03b92707d0b3fa2664adc1e5b99dfc8ee
jeklin feronika sinaga <99625360+jeklinferonikasinaga@users.noreply.github.com>
2023-11-10 12:33:36 +0800

    Day31 menampilkan angka 1-100 yang habis di bagi 3

---
+++ coding100hari/src/Day31.java
@@ -0,0 +1,22 @@
+package perbandingan;
+
+public class Day31 {
+    public static void main(String[] args) {
+        double x =1;
+        double y = 2;
+        double p =x +y;
+
+
+        if (p >= 4) {
+            double Q;
+            Q =y*y;
+            System.out.println("ya" +Q);
+        } else {
+            double Q;
+            Q = x/y;
+
+            System.out.println("tidak"+Q);
+        }
+
+    }
+}
