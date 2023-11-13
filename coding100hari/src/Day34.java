c8028b2b7af5e8edc8910f4761629532a7e165ed
jeklin feronika sinaga <99625360+jeklinferonikasinaga@users.noreply.github.com>
2023-11-13 19:31:42 +0800

    Day34

---
+++ coding100hari/src/Day34.java
@@ -0,0 +1,11 @@
+package aritmatika;
+
+public class Day34 {
+    public static void main(String[] args) {
+        double komputer =55000000;
+        double tabungan=1500000;
+
+
+        System.out.println(Math.ceil(komputer /tabungan));
+    }
+}
